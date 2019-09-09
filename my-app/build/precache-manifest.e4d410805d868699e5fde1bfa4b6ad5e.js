af2dbc5e95e619b97cf7afd49cb4116d929e585a
Vicki Shaw <vickishaw94@gmail.com>
2019-09-08 21:54:31 -0400

    gitignore remove build

---
+++ my-app/build/precache-manifest.e4d410805d868699e5fde1bfa4b6ad5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b70353bde28398372bf8ac9bab8b14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4864cb8debf340101950",
+    "url": "/static/css/main.66eeaa8b.chunk.css"
+  },
+  {
+    "revision": "8a5382c7d7c16c9ef896",
+    "url": "/static/js/2.2b6383ca.chunk.js"
+  },
+  {
+    "revision": "4864cb8debf340101950",
+    "url": "/static/js/main.428bec0e.chunk.js"
+  },
+  {
+    "revision": "85304a0d8147cfb7967e",
+    "url": "/static/js/runtime~main.077bb605.js"
+  }
+]);
